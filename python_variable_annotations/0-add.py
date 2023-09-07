1f62a7ea47b0a800e179e0b46f0e0f6e524d4c7e
FosterClark48 <fosterclark48@yahoo.com>
2023-09-06 21:29:01 -0500

    Type-annotated function add() that returns sum of 2 floats

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Type-Annotated function add() that returns sum of 2 floats """
+
+
+def add(a: float, b: float) -> float:
+    """ Add 2 floats & return their sum """
+    return a + b
