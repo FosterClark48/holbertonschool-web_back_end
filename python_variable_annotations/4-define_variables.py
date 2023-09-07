6cfc1fbaf7f99e9500ef20eadc3ed704da7b6d06
FosterClark48 <fosterclark48@yahoo.com>
2023-09-07 14:39:45 -0500

    This module defines and annotates four variables: `a`, `pi`, `i_understand_annotations`, and `school`.

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module defines and annotates four variables: `a`, `pi`,
+`i_understand_annotations`, and `school`.
+
+- `a` is an integer with a value of 1
+- `pi` is a float with a value of 3.14
+- `i_understand_annotations` is a boolean with a value of True
+- `school` is a string with a value of "Holberton"
+"""
+
+
+a: int = 1
+
+pi: float = 3.14
+
+i_understand_annotations: bool = True
+
+school: str = "Holberton"
