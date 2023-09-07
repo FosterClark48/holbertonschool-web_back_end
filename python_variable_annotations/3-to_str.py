3ac09d4a38e16f10675a4cc458422d4f3149ba7a
FosterClark48 <fosterclark48@yahoo.com>
2023-09-07 14:26:53 -0500

    This module provides a type-annotated function `to_str` that returns the string representation of the float.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+This module provides a type-annotated function `to_str` that returns
+the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+    """
+    return str(n)
