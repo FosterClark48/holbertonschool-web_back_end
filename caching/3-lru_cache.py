63e7cacbac1a0903e1592b7d980f02558417ff6f
FosterClark48 <fosterclark48@yahoo.com>
2023-09-17 23:02:58 -0500

    Inherits from BaseCaching and implements LRU caching system.

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+""" LRUCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    Inherits from BaseCaching and implements LRU caching system.
+    """
+    def __init__(self):
+        """ Initialize the LRUCache
+
+        Call the parent class initializer and create an item_order list.
+        """
+        super().__init__()
+        self.item_order = []
+
+    def put(self, key, item):
+        """ Store a key-value pair in the cache
+
+        This method stores a key-value pair in the cache_data dict and
+        updates the order list to maintain LRU characteristics. It evicts the
+        least recently used item if the cache reaches its maximum size.
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        if key in self.item_order:
+            self.item_order.remove(key)
+        self.item_order.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            evicted_key = self.item_order.pop(0)
+            del self.cache_data[evicted_key]
+            print(f"DISCARD: {evicted_key}")
+
+    def get(self, key):
+        """ Retrieve a value from the cache
+
+        This method retrieves a value based on a given key from the cache_data
+        dictionary and updates the order list
+        """
+        if key is None:
+            return None
+
+        value = self.cache_data.get(key, None)
+        if value is not None:
+            self.item_order.remove(key)
+            self.item_order.append(key)
+
+        return value
