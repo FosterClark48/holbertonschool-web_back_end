377679cf1d75dafc91e0deeaa888a74d31b5b9cc
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 17:11:53 +0000

    removed fetching the user line and the check for none line

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -57,11 +57,8 @@ class Auth:
 
     def destroy_session(self, user_id: int) -> None:
         """ Takes a user ID and sets its session ID to None """
-        if user_id is None:
-            return None
         try:
             self._db.find_user_by(user_id=user_id)
-            self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
 
