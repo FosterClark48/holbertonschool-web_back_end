367d7962b258b5a541581abbfbfb65bb4b070d69
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 17:08:17 +0000

    Added destroy_session() method that takes a user ID and sets its session ID to None

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -55,6 +55,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """ Takes a user ID and sets its session ID to None """
+        if user_id is None:
+            return None
+        try:
+            self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """This function receives a password string and returns its hashed version
