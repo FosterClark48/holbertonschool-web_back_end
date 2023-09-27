463d80e88878eede93a08a5f74c92067ac19965a
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 17:13:19 +0000

    Accidentally removed the wrong line of code! i removed the update_user line when i shouldve removed the find_user_by line

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -58,7 +58,7 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """ Takes a user ID and sets its session ID to None """
         try:
-            self._db.find_user_by(user_id=user_id)
+            self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
 
