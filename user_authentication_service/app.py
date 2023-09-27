53e6fcbfc1afb9813b5b42f963767a43a917e200
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 18:36:48 +0000

    Added "_token" to end of function name!! and added strict_slashes back and the 200 success code

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -69,14 +69,14 @@ def profile():
         abort(403)
 
 
-@app.route('/reset_password', methods=['POST'])
-def get_reset_password():
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
     """ Generate a reset password token """
     email = request.form.get('email')
 
     try:
         reset_token = AUTH.get_reset_passwor_token(email)
-        return jsonify({"email": email, "reset_token": reset_token})
+        return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
 
