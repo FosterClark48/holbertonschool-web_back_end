540fbab4efc60da12ca7b35c4314d8bd8dea7670
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 03:24:20 +0000

    Created auth.py file and added _hash_password method that receives a password and returns the hashed version as bytes, which are a salted hash of the password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Auth module """
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """This function reveibes a password string and returns its hashed version
+    as bytes. The returned bytes are a salted hash of the input password.
+    """
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
