ab37d5cf6da9f6f5757ee156246ede2e3106a4e0
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 04:10:35 +0000

    Created users() method that registers a user if its not already registered

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ This module contains a Flask app that serves a JSON response """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -9,6 +12,19 @@ app = Flask(__name__)
 def welcome():
     """ This serves a welcoming JSON response """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users():
+    """ Register a user if not already registered """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
