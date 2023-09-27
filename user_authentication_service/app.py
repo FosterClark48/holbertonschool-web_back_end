27e94c637c870affb5c5ad3aad5f8ff05f1484bf
FosterClark48 <fosterclark48@yahoo.com>
2023-09-27 16:36:21 +0000

    Added a login() function to respond to POST /sessions route / logs in to a session

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ This module contains a Flask app that serves a JSON response """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 
@@ -25,6 +25,21 @@ def users():
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Log in to a session """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
