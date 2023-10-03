6c42e5db0e897356c740bf11b698eb6b1504322c
FosterClark48 <fosterclark48@yahoo.com>
2023-10-03 21:08:34 +0000

    Test case for the function memoize

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -54,3 +54,30 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
         self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test case for the function memoize """
+    def test_memoize(self):
+        """ Test the memoize decorator """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_class = TestClass()
+
+        with patch.object(TestClass,
+                          'a_method',
+                          return_value=42) as mocked_a_method:
+            result_1 = test_class.a_property
+            result_2 = test_class.a_property
+
+            mocked_a_method.assert_called_once()
+
+            self.assertEqual(result_1, 42)
+            self.assertEqual(result_2, 42)
