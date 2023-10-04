86ceda1ef79a8246db3301653eda468d2d1eb883
FosterClark48 <fosterclark48@yahoo.com>
2023-10-04 23:38:50 +0000

    Setup and configured Babel and applied it to the app

---
+++ i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Basic Flask App Module w/ Babel setup """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration class for Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+# Apply config to app
+app.config.from_object(Config)
+
+# Instantiate Babel
+babel = Babel(app)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ Route for index page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
