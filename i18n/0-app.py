771f144e7ab2c2560382f365da5bd2195924f2fb
FosterClark48 <fosterclark48@yahoo.com>
2023-10-04 22:24:08 +0000

    Fixed pycodestyle and app.route args

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """ Route for index page """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
