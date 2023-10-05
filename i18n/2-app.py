9adb2d6125b571c443eb3caba4a8b6ec67081ce0
FosterClark48 <fosterclark48@yahoo.com>
2023-10-05 01:32:19 +0000

    Added localeselector decorator w/ get_locale method to determine the best language for a users browsers Accept-Language header and supported languages in my app LANGUAGES

---
+++ i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" Basic Flask App Module w/ Babel setup and Locale detections """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration class for Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# Apply config to app
+app.config.from_object(Config)
+
+# Instantiate Babel
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Determine best match w/ supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ Route for index page """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
