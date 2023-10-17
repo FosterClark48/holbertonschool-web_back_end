1c1219c4d69c5ebac76822ab14326c9599e6d044
FosterClark48 <fosterclark48@yahoo.com>
2023-10-17 03:59:02 +0000

    Added methods get, get_str, and get_int which all gets the values for a given key from Redis and returns the desired format

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -19,3 +19,21 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Union[str, bytes, int, float]:
+        """ Get the value for the given key from Redis """
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Get the value for the given key from Redis as a string """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Get the value for the given key from Redis as an integer"""
+        value = self._redis.get(key)
+        return int(value)
