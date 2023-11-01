bfa71f92abff92d70a6527407c91a96ad51a3507
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 13:49:55 -0500

    changed the output from logging to outputing a string

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
@@ -11,13 +11,12 @@ const app = http.createServer((req, res) => {
   } else if (req.url === '/students') {
     const database = process.argv[2];
 
-    countStudents(database)
-      .then((output) => {
-        res.end(`This is the list of our students\n${output}`);
-      })
-      .catch((error) => {
-        res.end(`This is the list of our students\n${error.message}`);
-      });
+    try {
+      const output = await countStudents(database);
+      res.end(`This is the list of our students\n${output}`);
+    } catch (error) {
+      res.end(`This is the list of our students\n${error.message}`);
+    }
   } else {
     res.end('Not found');
   }
