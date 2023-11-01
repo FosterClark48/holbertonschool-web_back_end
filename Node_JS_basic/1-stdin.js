dcbccb8e2cf1308f9a96d8130eef9c5fe2defffc
FosterClark48 <fosterclark48@yahoo.com>
2023-10-31 23:56:31 -0500

    fixed lint issues

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 // Program that listens for input from user, output's name, then displays closing message
 
-const readline = require('readline')
+const readline = require('readline');
 
 // Create interface for readline
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // Display initial message
@@ -14,7 +14,7 @@ console.log('Welcome to Holberton School, what is your name?');
 // Handle input
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
-  rl.close
+  rl.close();
 });
 
 // Handle close event
