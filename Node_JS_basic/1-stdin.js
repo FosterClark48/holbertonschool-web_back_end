1eea64c1e248c680a0081e8ccebe400148b70ae2
FosterClark48 <fosterclark48@yahoo.com>
2023-10-31 23:55:19 -0500

    Program that listens for input from user, output's name, then displays closing message

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+// Program that listens for input from user, output's name, then displays closing message
+
+const readline = require('readline')
+
+// Create interface for readline
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Display initial message
+console.log('Welcome to Holberton School, what is your name?');
+
+// Handle input
+rl.on('line', (input) => {
+  console.log(`Your name is: ${input}`);
+  rl.close
+});
+
+// Handle close event
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
