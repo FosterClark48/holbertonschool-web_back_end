b11167ff96c8f0d6a717d99a844e67ebb600e07e
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 17:36:09 -0500

    Task 2 - copy task 1 file and add 2 functioncs and call them

---
+++ queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,27 @@
+// Task 2 - copy task 1 file and add 2 functioncs and call them
+
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, reply) => {
+      console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
