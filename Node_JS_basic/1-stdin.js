8611a45c3f9feb70d93216a8315e7b26eab06383
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 00:23:25 -0500

    freaking typo in readable....

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 
 // Handle input
-process.stdin.on('readble', () => {
+process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
