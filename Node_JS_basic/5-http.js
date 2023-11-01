87148835f0efe89304298d3a9bdecadc37c9f1c4
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 14:06:38 -0500

    Had to change code to override console.log that pushes message into the 'output' array instead of printing to console

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -11,10 +11,22 @@ const app = http.createServer(async (req, res) => {
   } else if (req.url === '/students') {
     const database = process.argv[2];
 
+    // This array is used to store all the output messages that would normally go to the console
+    const output = ['This is the list of our students'];
+    // store original console.log function in originalConsoleLog so it can be restored later.
+    const originalConsoleLog = console.log;
+    // Override console.log w/ new function that, instead of printing to console,
+    // pushes message into 'output' array.
+    console.log = (message) => {
+      output.push(message);
+    };
+
     try {
-      const output = await countStudents(database);
-      res.end(`This is the list of our students\n${output}`);
+      await countStudents(database);
+      console.log = originalConsoleLog;
+      res.end(output.join('\n'));
     } catch (error) {
+      console.log = originalConsoleLog;
       res.end(`This is the list of our students\n${error.message}`);
     }
   } else {
