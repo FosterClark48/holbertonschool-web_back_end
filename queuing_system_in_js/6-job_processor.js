4545aa0ed9504050a0e5886436ea73b301ddd436
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 18:23:16 -0500

    Task 7 - Create the Job processor

---
+++ queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,15 @@
+// Task 7 - Create the Job processor
+
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+};
+
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message);
+  done();
+});
