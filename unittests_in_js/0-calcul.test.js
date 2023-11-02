6b96158f1c9e26dabecdf50dba6ebf3913b499ce
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 23:01:11 -0500

    Task 0 - Basic test with Mocha and Node assertion library

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,13 @@
+// Test Cases for Task 0
+
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber', () => {
+  it('should return the sum of rounded a and b', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
