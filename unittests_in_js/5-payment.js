ed6841498cc4fa00973fa55c7675c22e35ce2122
FosterClark48 <fosterclark48@yahoo.com>
2023-11-02 00:26:57 -0500

    Task 5 - Test cases for Hooks

---
+++ unittests_in_js/5-payment.js
@@ -0,0 +1,11 @@
+// Task 5 - Hooks
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
+
