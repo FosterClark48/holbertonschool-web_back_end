8f4c11536ea10443f97b7873fe72de86f659b8bb
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 23:08:49 -0500

    trying a different test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -9,6 +9,6 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(1.4, 3.2), 5);
+    assert.strictEqual(calculateNumber(0.1, 0.3), 0);
   });
 });
