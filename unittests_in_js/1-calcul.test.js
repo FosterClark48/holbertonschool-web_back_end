d965f939eec7d56b1995eccdd67eb8b1aa7037dc
FosterClark48 <fosterclark48@yahoo.com>
2023-11-01 23:23:41 -0500

    Task 1 - upgraded function and test file

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,37 @@
+// Test cases for Task 1
+
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('Type SUM', () => {
+    it('should return the sum of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.5, 3.7), 6);
+    });
+  });
+
+  describe('Type SUBTRACT', () => {
+    it('should return the difference of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('SUBTRACT', 3.7, 1.5), 2);
+    });
+  });
+
+  describe('Type DIVIDE', () => {
+    it('should return the division of rounded numbers', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      assert.strictEqual(calculateNumber('DIVIDE', 4.5, 1.4), 3);
+    });
+
+    it('should return Error when dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+
+  describe('Invalid type', () => {
+    it('should return Error for invalid type', () => {
+      assert.strictEqual(calculateNumber('INVALID', 1.4, 4.5), 'Error');
+    });
+  });
+});
